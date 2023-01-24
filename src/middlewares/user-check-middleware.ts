8945698fe250e7a1661f45f21645e023a39e0cba
ironPriest <iron.priest.mih@gmail.com>
2023-01-24 19:23:20 +0300

    #11 edit middleware

--- src/middlewares/user-check-middleware.ts
+++ src/middlewares/user-check-middleware.ts
@@ -1,14 +1,16 @@
 import {NextFunction, Request, Response} from "express";
 import {jwtUtility} from "../application/jwt-utility";
 import {usersService} from "../domain/users-service";
+import {resolveObjectURL} from "buffer";
 
 export const userCheckMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     console.log('headers -->', req.headers)
-    if (!req.headers.authorization) {
-        next()
+    const auth = req.headers.authorization
+    if (!auth) {
+        return next()
     }
 
-    const token = req.headers.authorization!.split(' ')[1]
+    const token = auth.split(' ')[1]
 
     const userId = await jwtUtility.getUserIdByToken(token)
     if (userId) {
