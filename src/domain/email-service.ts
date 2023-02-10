c6a67ac86a086ccfe5bc6462d74c6bef06e9aea8
ironPriest <iron.priest.mih@gmail.com>
2023-02-10 13:45:27 +0300

    #11 EmailService to class

--- src/domain/email-service.ts
+++ src/domain/email-service.ts
@@ -1,10 +1,12 @@
 import {emailManager} from "../managers/email-manager";
 
-export const emailService = {
+class EmailService {
     async register(email: string, subject: string, code: string) {
         await emailManager.sendRegistrationCode(email, subject, code)
-    },
+    }
     async passwordRecovery(email: string, subject: string, code: string) {
         await emailManager.passwordRecovery(email, subject, code)
     }
 }
+
+export const emailService = new EmailService()
