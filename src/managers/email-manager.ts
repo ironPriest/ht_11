a115ca9a70ae20c5dcd0155a9e084e46f5f625ef
ironPriest <iron.priest.mih@gmail.com>
2023-02-10 13:52:36 +0300

    #11 EmailManager to class

--- src/managers/email-manager.ts
+++ src/managers/email-manager.ts
@@ -1,10 +1,12 @@
 import {emailAdapter} from "../adapters/email-adapter";
 
-export const emailManager = {
+class EmailManager {
     async sendRegistrationCode(email: string, subject: string, code: string) {
         await emailAdapter.sendEmail(email, subject, code)
-    },
+    }
     async passwordRecovery(email: string, subject: string, code: string) {
         await emailAdapter.passwordRecovery(email, subject, code)
-    },
+    }
 }
+
+export const emailManager = new EmailManager()
